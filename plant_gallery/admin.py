834b2f64eebae818aed78f0ab4aaf6e40d4a384a
plantonmars <joesalinas@plantonmars.dev>
2020-03-11 14:34:47 -0400

    Added Item to admin

--- plant_gallery/admin.py
+++ plant_gallery/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Lighting, Type, Plant
+from item_system.models import Item
 
 admin.site.register(Lighting)
 admin.site.register(Type)
 admin.site.register(Plant)
+admin.site.register(Item)
